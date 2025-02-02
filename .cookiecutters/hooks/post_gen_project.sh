2a81909186b900930d980d2cf65a952c4c399d45
sebport0 <8371137+sebport0@users.noreply.github.com>
2025-02-02 10:29:39 -0300

    refactor(dsa-cpp): use link provided by input

--- .cookiecutters/hooks/post_gen_project.sh
+++ .cookiecutters/hooks/post_gen_project.sh
@@ -3,9 +3,11 @@
 name="{{cookiecutter.name}}"
 lang="{{cookiecutter.lang}}"
 source="{{cookiecutter.source}}"
-link="{{cookiecutter.link}}"
 
 if [ "${source,,}" == "leetcode" ]; then
+  link=$(echo "$name" | sed 's/^[0-9]*\. //')
+  link=${link// /-}
+  link="https://leetcode.com/problems/$(echo "$link" | tr "[:upper:]" "[:lower:]")"
   code_path=${name//". "/_}
   code_path=$(echo "$code_path" | tr " " "_" | tr "[:upper:]" "[:lower:]")
   code_path="/problems/leetcode/$code_path"
---
+++ .cookiecutters/{{cookiecutter.dir_name}}/{{cookiecutter.file_name}}.cpp
@@ -0,0 +1,19 @@
+// {{cookiecutter.link}}
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <sys/types.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+};
+
+int main() { return 0; }
